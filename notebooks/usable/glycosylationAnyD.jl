8c67820a5e87d3129983c7fd8321d4ced4630d3f
Christopher Revell <christopher.revell@manchester.ac.uk>
2024-09-06 18:31:41 +0100

    Tidying usable

---
+++ notebooks/usable/glycosylationAnyD.jl
@@ -0,0 +1,227 @@
+#%%
+# flux_νₑ = (diffusive_flux_ν + advective_flux_ν)
+# flux_νₑ = K₂*K₄.*Pν*∇ₑ*cᵥ - β*Pν*cₑ    where cᵥ is concentration over vertices, cₑ is concentration over edges 
+# cₑ = Aᵤₚ*cᵥ
+# flux_νₑ = (K₂*K₄.*Pν*∇ₑ - β*Pν*Aᵤₚ)*cᵥ
+# flux_xyₑ = Dₑ*hₑ*diffusive_flux_xy
+# flux_xyₑ = Dₑ*hₑ*Pxy*∇ₑ*cᵥ
+# ċ = aE∇⋅flux_νₑ + a∇⋅flux_xyₑ
+# ċ = a*E*∇⋅(K₂*K₄.*Pν*∇ₑ*cᵥ - β*Pν*Aᵤₚ*cᵥ) + a∇⋅(Dₑ*hₑ*Pxy*∇ₑ*cᵥ)
+# Dₑ constant over edges 
+# ċ = a*(E*∇⋅(K₂*K₄.*Pν*∇ₑ - β*Pν*Aᵤₚ) + 𝓓.*∇⋅(hₑ*Pxy*∇ₑ))*cᵥ
+
+# L = -W⁻¹*Aᵀ*𝓓*l⁻¹*A .+ W⁻¹*Aᵀ*V*Aᵤₚ # Express model as a matrix operator 
+
+
+# Cνν = W⁻¹*Aᵀ*Pν*l⁻¹*A
+# Cν = Aᵀ*l⁻¹*Pν*Aᵤₚ
+# flux_νₑ = (diffusive_flux_ν + advective_flux_ν)
+# flux_νₑ = K₂*K₄.*Pν*∇ₑ*cᵥ - β*Pν*cₑ    where cᵥ is concentration over vertices, cₑ is concentration over edges 
+# cₑ = Aᵤₚ*cᵥ
+# flux_νₑ = (K₂*K₄.*Pν*∇ₑ - β*Pν*Aᵤₚ)*cᵥ
+# flux_xyₑ = Dₑ*hₑ*diffusive_flux_xy
+# flux_xyₑ = Dₑ*hₑ*Pxy*∇ₑ*cᵥ
+# ċ = aE∇⋅flux_νₑ + a∇⋅flux_xyₑ
+# ċ = a*E*∇⋅(K₂*K₄.*Pν*∇ₑ*cᵥ - β*Pν*Aᵤₚ*cᵥ) + a∇⋅(Dₑ*hₑ*Pxy*∇ₑ*cᵥ)
+# Dₑ constant over edges 
+# ċ = a*(E*∇⋅(K₂*K₄.*Pν*∇ₑ - β*Pν*Aᵤₚ) + 𝓓.*∇⋅(hₑ*Pxy*∇ₑ))*cᵥ
+
+
+#
+
+
+using OrdinaryDiffEq
+using SparseArrays
+using UnPack
+using CairoMakie 
+using FromFile
+using DrWatson
+using Printf
+using SciMLOperators
+using DataFrames
+using Dates
+using Statistics
+
+@from "$(srcdir("MakeIncidenceMatrix.jl"))" using MakeIncidenceMatrix
+@from "$(srcdir("MakeWeightMatrices.jl"))" using MakeWeightMatrices
+@from "$(srcdir("Visualise.jl"))" using Visualise
+@from "$(srcdir("UsefulFunctions.jl"))" using UsefulFunctions
+@from "$(srcdir("CisternaWidth.jl"))" using CisternaWidth
+
+cisternaSeriesID = 1
+nSpatialDims = 2
+
+Ωperp = 100.0  # Lumen footprint area
+N     = 100         # Maximum polymer length 
+k_Cd  = 0.9 # Complex desorption rate
+k_Ca  = 1.1 # Complex adsorption rate
+k_Sd  = 0.9 # Substrate desorption rate
+k_Sa  = 1.1 # Substrate adsorption rate
+k₁    = 1.0   # Complex formation forward reaction rate 
+k₂    = 0.6   # Complex dissociation reverse reaction rate 
+k₃    = 1.1   # Product formation
+k₄    = 0.6  # Product dissociation 
+E_0   = 1.0
+𝓒     = 100.0
+𝓢     = 100.0
+D_C   = 1.0  # Monomer/polymer diffusivity
+D_S   = 1.0  # Substrate diffusivity
+Tᵣstar= 100.0  # Release time
+ϕ     = 0.5
+
+# PDE discretisation parameters 
+Nν       = 31             # Number of discretisation points in polymerisation space
+Nx       = 31             # Number of discretisation points in space
+Ny       = 31             # Number of discretisation points in space
+Nghost   = 1           # Number of ghost points on each side of the domain 
+Nνplus   = Nν+2*Nghost # Number of discretised points including ghost points 
+Nxplus   = Nx+2*Nghost # Number of discretised points including ghost points
+Nyplus   = Ny+2*Nghost # Number of discretised points including ghost points
+
+Ngrid = 101
+nSpatialDims == 2 ? dimsPlus = [Nνplus, Nxplus] : dimsPlus = [Nνplus, Nxplus, Nyplus]
+nSpatialDims == 2 ? dimsReal = [Nν, Nx] : dimsReal = [Nν, Nx, Ny]
+
+# Generate xMax and width profile from data or function 
+# xMax, mat_h = hFromData(dimsPlus; cisternaSeriesID=1)
+xMax, mat_h, xs = hFromFunction(dimsPlus)
+dx   = xs[2]-xs[1]
+nSpatialDims > 2 ? yMax = xMax : yMax = 2
+ys   = collect(range(0.0, yMax, Nyplus))
+dy   = ys[2]-ys[1]
+νMax = 1.0
+νs   = collect(range(0.0, νMax, Nνplus)) # Positions of discretised vertices in polymerisation space 
+dν   = νs[2]-νs[1]
+nSpatialDims == 2 ? spacing  = [dν, dx] : spacing  = [dν, dx, dy]
+
+h₀ = mean(selectdim(mat_h, 2, 1:Nxplus))
+
+𝓔    = 2*Ωperp*E_0   # Total enzyme mass
+K₃  = k₃/k₁    # Non-dimensionalised product formation rate
+K₄  = k₄/k₁    # Non-dimensionalised prodict dissociation rate
+δ_C = π*D_C/(k₁*𝓔)
+δ_S = π*D_S/(k₁*𝓔)
+Tᵣ  = k₁*𝓔*Tᵣstar/(2*Ωperp)
+Ω     = h₀*Ωperp         # Lumen volume
+α_C = (k_Cd*Ω)/(2*k_Ca*Ωperp) # Balance of complex in bulk to complex on membrane       units of m²?
+α_S = (k_Sd*Ω)/(2*k_Sa*Ωperp) # Balance of substrate in bulk to substrate on membrane   units of m²?
+C_b  = 𝓒/Ω 
+S_b  = 𝓢/Ω 
+C_0 = C_b*h₀/(2*(1+α_C))      # Early surface monomer concentration
+S_0 = S_b*h₀/(2*(1+α_S))      # Early surface substrate concentration 
+K₂  = k₂/(k₁*C_0)              # (k₂/(k₁*C_b))*((2*k_Ca*Ωperp + k_Cd*Ω)/(k_Ca*Ω)) # Non-dimensionalised complex formation net reaction rate
+σ   = S_0/C_0                         #(k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω))
+ϵ   = E_0/C_0                  # 𝓔*(2*k_Ca*Ωperp + k_Cd*Ω) / (2*k_Ca*C_b*Ωperp)
+𝓓   = α_C*δ_C*N^2*(K₂ + σ*K₃)
+β = N*(σ*K₃ - K₂*K₄)
+
+λ = (𝓢/(2*Ωperp))*(k₁*k₃/(k₂*k₄))
+@show λ
+
+#%%
+
+# Create directory for run data labelled with current time.
+paramsName = @savename cisternaSeriesID K₂ K₃ K₄ α_C δ_C σ N Tᵣ nSpatialDims
+folderName = "$(paramsName)_$(Dates.format(Dates.now(),"yy-mm-dd-HH-MM-SS"))"
+# Create frames subdirectory to store system state at each output time
+subFolder = ""
+mkpath(datadir("sims",subFolder,folderName))
+
+#%%
+
+A   = makeIncidenceMatrix3D(dimsPlus)
+Ā   = abs.(A)
+Aᵀ  = transpose(A)
+Aᵤₚ = dropzeros((Ā-A).÷2)
+
+nVerts  = prod(dimsPlus)      # Total number of vertices 
+dimEdgeCount = Int64[]
+for i=1:length(dimsPlus)
+    push!(dimEdgeCount, (dimsPlus[i]-1)*prod(dimsPlus[1:i-1])*prod(dimsPlus[i+1:end]))
+end
+nEdges  = sum(dimEdgeCount)   # Total number of edges over all dimensions 
+
+# Ghost point masks
+ghostVertexMaskVec = makeGhostVertexMask(dimsPlus)
+ghostVertexMaskSparse = spdiagm(ghostVertexMaskVec)
+ghostEdgeMaskVec = makeGhostEdgeMask(dimsPlus)
+ghostEdgeMaskSparse = spdiagm(ghostEdgeMaskVec)
+
+# Matrices for picking out ν and xy directions in derivatives 
+Pν  = ghostEdgeMaskSparse*spdiagm(vcat(ones(Int64, dimEdgeCount[1]), zeros(Int64, dimEdgeCount[2]+dimEdgeCount[3])))   # Diagonal sparse matrix to exclude all xy edges 
+Pxy  = ghostEdgeMaskSparse*spdiagm(vcat(zeros(Int64, dimEdgeCount[1]), ones(Int64, dimEdgeCount[2]+dimEdgeCount[3])))   # Diagonal sparse matrix to exclude all ν edges 
+
+# Weights
+W = vertexVolumeWeightsMatrix(dimsPlus, spacing)
+W⁻¹ =  vertexVolumeWeightsInverseMatrix(dimsPlus, spacing)
+l⁻¹ = edgeLengthInverseMatrix(dimsPlus, spacing)
+
+∇ₑ = l⁻¹*A       # Gradient operator giving gradient on each edge
+∇cdot = -W⁻¹*Aᵀ  # Divergence operator giving divergence on each vertex calculated from edges 
+
+# Diffusivity field over edges 
+# Set no-flux boundary conditions by enforcing zero diffusivity in edges connection ghost points
+Aperpₑ = edgePerpendicularAreaMatrix(dimsPlus, spacing)
+𝓓ₑ     = 𝓓.*ghostEdgeMaskSparse*Aperpₑ # Sparse diagonal matrix of diffusivities over edges 
+
+# Diagonal matrices of compartment thickness h over all vertices hᵥ
+# Also diagonal matrix of thickness over edges, formed by taking mean of h at adjacent vertices 0.5.*Ā*hᵥ
+hᵥ_vec = reshape(mat_h, nVerts)         # Cisternal thickness evaluated over vertices 
+hₑ_vec = 0.5.*Ā*hᵥ_vec                  # Cisternal thickness evaluated over edges (mean of adjacent vertices)
+hᵥ = spdiagm(hᵥ_vec)                    # Cisternal thickness over vertices, as a sparse diagonal matrix
+hₑ = spdiagm(hₑ_vec)                    # Cisternal thickness over edges, as a sparse diagonal matrix
+aᵥ = spdiagm(1.0./(1.0 .+ α_C.*hᵥ_vec)) # Prefactor 1/(1+α_C*hᵥ(x)) evaluated over vertices, packaged into a sparse diagonal matrix for convenience
+aₑ = spdiagm(1.0./(1.0 .+ α_C.*hₑ_vec)) # Prefactor 1/(1+α_C*hₑ(x)) evaluated over edges, packaged into a sparse diagonal matrix for convenience
+
+# Initial conditions using Gaussian
+u0fun(x, μx, σx, y, μy, σy, z, μz, σz) = exp(-(x-μx)^2/σx^2 - (y-μy)^2/σy^2 - (z-μz)^2/σz^2)
+μνu0 = 0.0; σνu0 = νMax/10.0
+μxu0 = xMax/2.0; σxu0 = 10.0*xMax
+μyu0 = xMax/2.0; σyu0 = 10.0*xMax
+uMat = zeros(Float64, dimsPlus...)
+for yy=1:dimsPlus[3], xx=1:dimsPlus[2], νν=1:dimsPlus[1]
+    uMat[νν, xx, yy] = u0fun(νs[νν], μνu0, σνu0, xs[xx], μxu0, σxu0, ys[yy], μyu0, σyu0)
+end
+u0 = reshape(uMat, nVerts)
+u0[ghostVertexMaskVec.!=true] .= 0.0
+# For integration to normalise the number of monomers, we need to multiply the concentration at each point by the ν value of that point
+νMat = ones(dimsPlus...)
+for ii=1:dimsPlus[1]
+    selectdim(νMat, 1, ii) .*= (ii-1)
+    # νMat[ii,:].*=(ii-1)
+end
+νSparse = spdiagm(reshape(νMat, nVerts))
+integ = sum(ghostVertexMaskSparse*W*νSparse*u0)
+u0 .*= 𝓒/integ
+
+# Set value of Fₑ at each point in space
+matFₑ = ones(Float64, dimsPlus[2:end]...)
+for i=2:length(dimsPlus)
+    selectdim(matFₑ, i-1, 1) .= 0.0
+    selectdim(matFₑ, i-1, dimsPlus[i]) .= 0.0
+end
+integF = prod(spacing[2:end])*sum(matFₑ)
+# Ensure integral of Fₑ over space is π
+matFₑ .*= π/integF
+matE = zeros(dimsPlus[2:end]...)
+Esparse = spzeros(nVerts, nVerts)
+E!(u0, dimsPlus, Esparse, matE, matFₑ, K₂, dν)
+
+# PDE operator components
+L1 = aᵥ*∇cdot*(K₂*K₄.*Pν*∇ₑ - β.*Pν*Aᵤₚ)
+L2 = aᵥ*∇cdot*(hₑ*Pxy*𝓓ₑ*∇ₑ)
+p = (L1=L1, L2=L2, u0=u0, dimsPlus=dimsPlus, Esparse=Esparse, matE=matE, matFₑ=matFₑ, K₂=K₂, dν=dν)
+L = MatrixOperator(Esparse*L1.+L2, update_func! = updateOperator!)
+
+prob = ODEProblem(L, u0, (0.0, Tᵣ), p)
+sol = solve(prob, Vern9(), saveat=Tᵣ/100.0, progress=true)
+
+#%%
+
+println("finished sim")
+
+if nSpatialDims==1
+    u2d = [u[:,:,2] for u in sol.u]
+    concentrationSurfaceMovie(u2d, sol.t, xs, νs, dimsReal, Nghost, ghostVertexMaskVec; subFolder=subFolder, folderName=folderName)
+end
+
---
+++ notebooks/usable/productionParameterSweepSymbolic.jl
@@ -0,0 +1,262 @@
+using Symbolics
+using CairoMakie
+using Latexify
+
+@variables α_C α_S k_Ca k_Cd k_Sa k_Sd Ω Ωperp h₀ L₀ k₁ k₂ k₃ k₄ K₂ K₃ K₄ C_b 𝓒 C_0 S_b 𝓢 S_0 σ E_0 𝓔 ϵ β N δ_C D_C δ_S D_S 𝓓 Tᵣ Tᵣstar Δ ϕ 𝓟star
+
+lhs(e) = e.lhs
+rhs(e) = e.rhs
+
+expressions = [
+    C_b ~ 𝓒/Ω,
+    S_b ~ 𝓢/Ω,
+    Ω ~ Ωperp*h₀,
+    α_C ~ (k_Cd*Ω)/(2*k_Ca*Ωperp),
+    α_S ~ (k_Sd*Ω)/(2*k_Sa*Ωperp),
+    K₂ ~ (k₂/(k₁*C_b))*((2*k_Ca*Ωperp + k_Cd*Ω)/(k_Ca*Ω)),
+    K₃ ~ k₃/k₁,
+    K₄ ~ k₄/k₁,
+    L₀ ~ sqrt(π)*Ω / (Ωperp)^(1.5),
+    C_0 ~ C_b*h₀/(2*(1+α_C)),
+    S_0 ~ S_b*h₀/(2*(1+α_S)),
+    σ ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω)),
+    𝓔 ~ 2*E_0*Ωperp,
+    σ ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω)),
+    ϵ ~ 𝓔*(2*k_Ca*Ωperp + k_Cd*Ω) / (2*k_Ca*C_b*Ωperp),
+    β ~ N*(σ*K₃ - K₂*K₄),
+    δ_C ~ π*D_C/(k₁*𝓔),
+    δ_S ~ π*D_S/(k₁*𝓔),
+    𝓓 ~ α_C*δ_C*N^2*(K₂ + σ*K₃),
+    Tᵣ ~ (k₁*𝓔*Tᵣstar)/(2*Ωperp),
+    β ~ N*(σ*K₃ - K₂*K₄),
+    Δ ~ k₁*𝓒/(2*k₂*Ωperp),
+]
+
+𝓟star = π/(2*ϕ) * (α_C*𝓒)/((1+α_C)^2) * (k₁*𝓔)/(2*Ωperp) * K₂/(1+K₂) * (σ*K₃-K₂*K₄)/(N*(K₂+σ*K₃)) * (1/Tᵣ)
+expr = 𝓟star ~ π/(2*ϕ) * (α_C*𝓒)/((1+α_C)^2) * (k₁*𝓔)/(2*Ωperp) * K₂/(1+K₂) * (σ*K₃-K₂*K₄)/(N*(K₂+σ*K₃)) * (1/Tᵣ)
+
+sub1 = substitute(𝓟star, Dict(lhs.(expressions).=>rhs.(expressions)))
+sub2 = substitute(sub1, Dict(lhs.(expressions).=>rhs.(expressions)))
+sub3 = substitute(sub2, Dict(lhs.(expressions).=>rhs.(expressions)))
+
+
+valuesDict = Dict(
+    Ωperp => 100.0,
+    N     => 100,
+    k_Cd => 0.9,
+    k_Ca => 1.1,
+    k_Sd => 0.9,
+    k_Sa => 1.1,
+    k₁   => 1.0,
+    k₂   => 0.6,
+    k₃   => 1.1,
+    k₄   => 0.6,
+    E_0 => 1.0,
+    𝓒 => 100.0,
+    𝓢 => 100.0,
+    D_C  => 1.0,
+    D_S  => 1.0,
+    Tᵣstar  => 50.0,
+    ϕ => 0.5,
+)
+
+args = [h₀ Ωperp N k₁ k₂ k₃ k₄ 𝓢 k_Sa k_Sd 𝓒 k_Ca k_Cd ϕ Tᵣstar]
+
+𝓟starFunc = eval(build_function(sub3, args...))
+
+
+
+Ωperp = 100.0  # Lumen footprint area
+N     = 100         # Maximum polymer length 
+k_Cd = 0.9 # Complex desorption rate
+k_Ca = 1.1 # Complex adsorption rate
+k_Sd = 0.9 # Substrate desorption rate
+k_Sa = 1.1 # Substrate adsorption rate
+k₁   = 1.0   # Complex formation forward reaction rate 
+k₂   = 0.6   # Complex dissociation reverse reaction rate 
+k₃   = 1.1   # Product formation
+k₄   = 0.6  # Product dissociation 
+E_0 = 1.0
+𝓒 = 100.0
+𝓢 = 100.0
+D_C  = 1.0  # Monomer/polymer diffusivity
+D_S  = 1.0  # Substrate diffusivity
+Tᵣstar  = 100.0  # Release time
+ϕ = 0.5
+
+
+𝓟starFunc(1.1, Ωperp, N, k₁, k₂, k₃, k₄, 𝓢, k_Sa, k_Sd, 𝓒, k_Ca, k_Cd, ϕ, Tᵣstar)
+
+
+# 𝓟starVal = substitute(sub3, valuesDict)
+
+h₀s = collect(0.1:0.1:3.0)
+
+# Ps = zeros(length(h₀s))
+Ps = 𝓟starFunc.(h₀s, Ωperp, N, k₁, k₂, k₃, k₄, 𝓢, k_Sa, k_Sd, 𝓒, k_Ca, k_Cd, ϕ, Tᵣstar)
+
+
+for i=1:length(h₀s)
+    num = substitute(𝓟starVal, h₀=>h₀s[i])
+    Ps[i] = num.val
+end
+
+fig = CairoMakie.Figure(size=(500,500))
+ax = Axis(fig[1,1])
+ax.xlabel = "h₀"
+ax.ylabel = L"𝓟^*"
+ylims!(ax, (0.0,maximum(Ps)))
+xlims!(ax, (0.0,maximum(h₀s)))
+lines!(ax, h₀s, Ps)
+display(fig)
+
+save("parameterSweep.png",fig)
+
+
+#%%
+
+expressions2 = []
+for i=1:length(expressions)
+    innr = [expressions[i].rhs]
+    for exp in expressions
+        innr[1] = substitute(innr[1], Dict(lhs.(expressions).=>rhs.(expressions)))
+    end
+    push!(expressions2, expressions[i].lhs~innr[1])
+end
+
+# substitute.(expressions2, valuesDict)
+tst = [substitute(exp, valuesDict) for exp in expressions2]
+#%%
+
+
+render(latexify(sub3))
+
+
+
+#%%
+
+# expr1 = Ω ~ Ωperp*h₀
+# expr2 = α_C ~ (k_Cd*Ω)/(2*k_Ca*Ωperp)                          # Balance of complex in bulk to complex on membrane
+# expr3 = α_S ~ (k_Sd*Ω)/(2*k_Sa*Ωperp)                          # Balance of substrate in bulk to substrate on membrane
+# expr4 = K₂ ~ (k₂/(k₁*C_b))*((2*k_Ca*Ωperp + k_Cd*Ω)/(k_Ca*Ω)) # Non-dimensionalised complex formation net reaction rate k₂/(k₁*C_0)
+# expr5 = K₃ ~ k₃/k₁                                            # Non-dimensionalised product formation rate
+# expr6 = K₄ ~ k₄/k₁                                            # Non-dimensionalised prodict dissociation rate
+# # expr h₀ ~ Ω/Ωperp                                          # Mean thickness 
+# expr7 = L₀ ~ sqrt(π)*Ω / (Ωperp)^(1.5)                        # Mean radius 
+# # expr C_b~ 𝓒/Ω                     # Initial monomer bulk concentration 
+# # expr S_b~ 𝓢/Ω                     # Initial substrate mass
+# expr8 = C_0 ~ C_b*h₀/(2*(1+α_C))        # Early surface monomer concentration
+# expr9 = S_0 ~ S_b*h₀/(2*(1+α_S))      # Early surface substrate concentration 
+# # expr E_0~ 𝓔/(2*Ωperp)             # Total enzyme mass
+# expr10 = 𝓒  ~ C_b*Ω                     # Initial monomer mass
+# expr11 = σ  ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω))
+# expr12 = 𝓢  ~ S_b*Ω                     # Initial substrate mass
+# expr13 = 𝓔  ~ 2*E_0*Ωperp               # Total enzyme mass
+# expr14 = σ  ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω)) # S_0/C_0
+# expr15 = ϵ  ~ 𝓔*(2*k_Ca*Ωperp + k_Cd*Ω) / (2*k_Ca*C_b*Ωperp) # E_0/C_0
+# expr16 = β  ~ N*(σ*K₃ - K₂*K₄)
+# expr17 = δ_C ~ π*D_C/(k₁*𝓔)
+# expr18 = δ_S ~ π*D_S/(k₁*𝓔)
+# expr19 = 𝓓  ~ α_C*δ_C*N^2*(K₂ + σ*K₃)
+# expr20 = Tᵣ ~ (k₁*𝓔*Tᵣstar)/(2*Ωperp)
+# expr21 = β ~ N*(σ*K₃ - K₂*K₄)
+# expr22 = Δ  ~ k₁*𝓒/(2*k₂*Ωperp)
+
+# Ω ~ Ωperp*h₀
+# α_C ~ (k_Cd*Ω)/(2*k_Ca*Ωperp)                          # Balance of complex in bulk to complex on membrane
+# α_S ~ (k_Sd*Ω)/(2*k_Sa*Ωperp)                          # Balance of substrate in bulk to substrate on membrane
+# K₂ ~ (k₂/(k₁*C_b))*((2*k_Ca*Ωperp + k_Cd*Ω)/(k_Ca*Ω)) # Non-dimensionalised complex formation net reaction rate k₂/(k₁*C_0)
+# K₃ ~ k₃/k₁                                            # Non-dimensionalised product formation rate
+# K₄ ~ k₄/k₁                                            # Non-dimensionalised prodict dissociation rate
+# # expr h₀ ~ Ω/Ωperp                                          # Mean thickness 
+# L₀ ~ sqrt(π)*Ω / (Ωperp)^(1.5)                        # Mean radius 
+# # expr C_b~ 𝓒/Ω                     # Initial monomer bulk concentration 
+# # expr S_b~ 𝓢/Ω                     # Initial substrate mass
+# C_0 ~ C_b*h₀/(2*(1+α_C))        # Early surface monomer concentration
+# S_0 ~ S_b*h₀/(2*(1+α_S))      # Early surface substrate concentration 
+# # expr E_0~ 𝓔/(2*Ωperp)             # Total enzyme mass
+# 𝓒  ~ C_b*Ω                     # Initial monomer mass
+# σ  ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω))
+# 𝓢  ~ S_b*Ω                     # Initial substrate mass
+# 𝓔  ~ 2*E_0*Ωperp               # Total enzyme mass
+# σ  ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω)) # S_0/C_0
+# ϵ  ~ 𝓔*(2*k_Ca*Ωperp + k_Cd*Ω) / (2*k_Ca*C_b*Ωperp) # E_0/C_0
+# β  ~ N*(σ*K₃ - K₂*K₄)
+# δ_C ~ π*D_C/(k₁*𝓔)
+# δ_S ~ π*D_S/(k₁*𝓔)
+# 𝓓  ~ α_C*δ_C*N^2*(K₂ + σ*K₃)
+# Tᵣ ~ (k₁*𝓔*Tᵣstar)/(2*Ωperp)
+# β ~ N*(σ*K₃ - K₂*K₄)
+# Δ  ~ k₁*𝓒/(2*k₂*Ωperp)
+
+
+# @variables α_C α_S k_Ca k_Cd k_Sa k_Sd Ω Ωperp h₀ L₀ k₁ k₂ k₃ k₄ K₂ K₃ K₄ C_b 𝓒 C_0 S_b 𝓢 S_0 σ E_0 𝓔 ϵ β N δ_C D_C δ_S D_S 𝓓 Tᵣ Tᵣstar Δ ϕ 𝓟star
+
+# Ω = Ωperp*h₀
+# C_b = 𝓒/Ω
+# S_b = 𝓢/Ω 
+# α_C = (k_Cd*Ω)/(2*k_Ca*Ωperp)                          
+# α_S = (k_Sd*Ω)/(2*k_Sa*Ωperp)                          
+# K₂ = (k₂/(k₁*C_b))*((2*k_Ca*Ωperp + k_Cd*Ω)/(k_Ca*Ω)) 
+# K₃ = k₃/k₁                                            
+# K₄ = k₄/k₁                                            
+# # expr h₀= Ω/Ωperp                                    
+# L₀ = sqrt(π)*Ω / (Ωperp)^(1.5)                        
+# # expr C_b~ 𝓒/Ω                     
+# # expr S_b~ 𝓢/Ω                     
+# C_0 = C_b*h₀/(2*(1+α_C))        
+# S_0 = S_b*h₀/(2*(1+α_S))      
+# # expr E_0~ 𝓔/(2*Ωperp)             
+# # 𝓒 = C_b*Ω                    
+# σ = (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω))
+# # 𝓢 = S_b*Ω                     
+# 𝓔 = 2*E_0*Ωperp               
+# σ = (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω)) # S_0/C_0
+# ϵ = 𝓔*(2*k_Ca*Ωperp + k_Cd*Ω) / (2*k_Ca*C_b*Ωperp) # E_0/C_0
+# β = N*(σ*K₃ - K₂*K₄)
+# δ_C = π*D_C/(k₁*𝓔)
+# δ_S = π*D_S/(k₁*𝓔)
+# 𝓓 = α_C*δ_C*N^2*(K₂ + σ*K₃)
+# Tᵣ = (k₁*𝓔*Tᵣstar)/(2*Ωperp)
+# β = N*(σ*K₃ - K₂*K₄)
+# Δ = k₁*𝓒/(2*k₂*Ωperp)
+
+
+# 𝓟starSimpl = simplify(sub1)
+
+
+# expressions0 = [
+#     K₃ ~ k₃/k₁,
+#     K₄ ~ k₄/k₁,
+#     𝓔 ~ 2*E_0*Ωperp,
+#     Δ ~ k₁*𝓒/(2*k₂*Ωperp),
+#     Ω ~ Ωperp*h₀,
+# ]
+
+# expressions1 = [
+#     α_C ~ (k_Cd*Ω)/(2*k_Ca*Ωperp),
+#     α_S ~ (k_Sd*Ω)/(2*k_Sa*Ωperp),
+#     L₀ ~ sqrt(π)*Ω / (Ωperp)^(1.5),
+#     δ_C ~ π*D_C/(k₁*𝓔),
+#     δ_S ~ π*D_S/(k₁*𝓔),
+#     Tᵣ ~ (k₁*𝓔*Tᵣstar)/(2*Ωperp),
+#     C_b ~ 𝓒/Ω,
+#     S_b ~ 𝓢/Ω,
+# ]
+
+# expressions2 = [
+#     K₂ ~ (k₂/(k₁*C_b))*((2*k_Ca*Ωperp + k_Cd*Ω)/(k_Ca*Ω)),
+#     C_0 ~ C_b*h₀/(2*(1+α_C)),
+#     S_0 ~ S_b*h₀/(2*(1+α_S)),
+#     σ ~ (k_Sa*S_b*(2*k_Ca*Ωperp + k_Cd*Ω)) / (k_Ca*C_b*(2*k_Sa*Ωperp + k_Sd*Ω)),
+#     ϵ ~ 𝓔*(2*k_Ca*Ωperp + k_Cd*Ω) / (2*k_Ca*C_b*Ωperp), # E_0/C_0
+#     β ~ N*(σ*K₃ - K₂*K₄),
+#     𝓓 ~ α_C*δ_C*N^2*(K₂ + σ*K₃),
+# ]
+# @variables k_Ca k_Cd k_Sa k_Sd Ωperp h₀ k₁ k₂ k₃ k₄ C_b S_b E_0 N D_C D_S Tᵣstar ϕ
+# @variables Ω(Ωperp,h₀) α_C(k_Cd,Ω,k_Ca,Ωperp) α_S(Ω,k_Sa,Ωperp) L₀(Ω,Ωperp) 
+# @variables K₂(k₁,k₂,C_b,k_Ca,Ωperp,k_Cd,Ω) K₃(k₁,k₃) K₄(k₁,k₄) 𝓒(C_b,Ω) C_0(C_b,h₀,α_C) 𝓢(S_b,Ω) S_0(S_b,h₀,α_S) 
+# @variables σ(S_b,k_Cd,k_Ca,C_b,k_Sa,k_Sd,Ωperp,Ω) 𝓔(E_0,Ωperp) 
+# @variables ϵ(𝓔,k_Cd,k_Ca,C_b,Ω,Ωperp) β(N,σ,K₃,K₂,K₄) 
+# @variables δ_C(D_C,k₁,𝓔) δ_S(D_S,k₁,𝓔) 𝓓(α_C,δ_C,N,K₂,σ,K₃) Tᵣ(k₁,𝓔,Tᵣstar,Ωperp) Δ(k₁,𝓒,k₂,Ωperp) 
+# @variables 𝓟star(ϕ,𝓒,α_C,k₁,𝓔,Ωperp,K₂,K₄,N,K₂,σ,K₃,Tᵣ)
